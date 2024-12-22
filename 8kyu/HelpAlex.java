7274f8823451de2057e576ecd28dbf004312c920
EH <eito2307@gmail.com>
2024-12-21 20:04:43 -0500

    Deleted HelpAlex.java

--- 8kyu/HelpAlex.java
+++
@@ -1,9 +0,0 @@
-public class HelpAlex{
-    public static String hoopCount(int n){
-     if(n>=10){
-       return "Great, now move on to tricks";
-     }else{
-       return "Keep at it until you get it";
-     }
-    }
-  }
