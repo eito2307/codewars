2111fb99c49dbf5e12f937879ad3b8bd44fce7b1
EH <eito2307@gmail.com>
2024-12-21 19:59:44 -0500

    Keep up The hoop in Java

---
+++ 8kyu/HelpAlex.java
@@ -0,0 +1,9 @@
+public class HelpAlex{
+    public static String hoopCount(int n){
+     if(n>=10){
+       return "Great, now move on to tricks";
+     }else{
+       return "Keep at it until you get it";
+     }
+    }
+  }
