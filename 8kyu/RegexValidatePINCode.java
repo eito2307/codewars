413468c9b24bdbfd97110df6d51c5f4062c72a52
EH <eito2307@gmail.com>
2024-12-22 21:04:49 -0500

    Regex Validate PIN Code

--- 8kyu/RegexValidatePINCode.java
+++ 8kyu/RegexValidatePINCode.java
@@ -24,3 +24,13 @@ Examples (Input --> Output)
     }
   
   }
+  /*OR */
+  import java.util.regex.*;
+
+public class Solution {
+
+  public static boolean validatePin(String pin) {
+    return pin.matches("\\d{4}|\\d{6}");
+  }
+
+}
