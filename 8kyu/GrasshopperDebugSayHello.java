c23135b12539c6ff56586580914d6d098b9c53fb
EH <eito2307@gmail.com>
2024-12-23 20:06:31 -0500

    GrasshopperDebugSayHello

---
+++ 8kyu/GrasshopperDebugSayHello.java
@@ -0,0 +1,17 @@
+/*
+ * Debugging sayHello function
+The starship Enterprise has run into some problem when creating a program to greet everyone as they come aboard. It is your job to fix the code and get the program working again!
+
+Example output:
+
+Hello, Mr. Spock
+Grasshopper - Debug sayHello
+
+ */
+public class GrassHopper {
+
+    public static String sayHello(String name) {
+
+        return "Hello, "+name; 
+    }
+}
